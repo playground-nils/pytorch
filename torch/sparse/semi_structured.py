487dbf5777478d586407c4569b2a1f59bfb12305
Zihao Liu <zihaoliu@meta.com>
2026-05-15 17:20:59 -0700

    Optimize for layernorm + sigmoid epilogue by prodiving ideal input shape to layernorm after sparse matmul (#183472)
    Summary:
    
    We noticed that laynorm + sigmoid has less latency in dense version compared to sparse matmul + laynorm + sigmoid.
    
    Diving into it, it was because the laynorm + sigmoid will be fused together in dense version but not sparse version. I believe this is because we did not provide an ideal input form for layernorm -- continuous memory layout at the last dimension. 
    
    I believe this makes sense to always provide a continuous memory layout to subsequent kernels in order to avoid unexpected performance issue. Therefore, I'd like to change the default behavior of semi-tensor addmm/mm, to always return a continuous layout output.
    
    
    A bench mark result as follow: 
    Focus on 4 columns
    EpiDc:  (dense) LayerNorm + sigmoid (continuous input)
    EpiDNc: (dense) LayerNorm + sigmoid (non-continuous input)
    EpiSpNc: (sparse) LayerNorm + sigmoid (non-continuous input)
    EpiSpC: (sparse) LayerNorm + sigmoid (continuous input)
    
         M        N        K |     EpiDc   EpiDNc   EpiSpNc   EpiSpC
    
         8     6144   141312 |     39.2     55.8     50.1     31.3
        32     6144   141312 |     46.2     62.0     52.3     36.9
       128     6144   141312 |     36.6     51.3     51.9     37.9
       256     6144   141312 |     35.9     50.0     50.2     35.9
       512     6144   141312 |     35.4     50.7     50.7     35.5
      1024     6144   141312 |     35.6     51.0     52.2     36.4
      2048     6144   141312 |     52.8     76.4     77.5     53.3
    
         8    32768     6144 |     40.1     46.2     45.2     40.0
        32    32768     6144 |     41.2     49.1     49.4     41.3
       128    32768     6144 |     44.2     73.4     73.1     43.9
       256    32768     6144 |     76.3    120.6    120.9     76.6
       512    32768     6144 |    111.9    213.8    219.1    112.2
      1024    32768     6144 |    184.1    392.5    390.7    183.0
      2048    32768     6144 |    329.3    752.7    739.0    326.4
    
         8   141312     1024 |    152.9    164.5    164.6    153.2
        32   141312     1024 |    166.4    184.4    184.3    166.4
       128   141312     1024 |    325.9    451.9    451.8    325.1
       256   141312     1024 |    384.2    637.1    635.9    384.1
       512   141312     1024 |    488.7   1006.2   1007.5    486.5
      1024   141312     1024 |    758.5   1826.1   1820.3    762.7
      2048   141312     1024 |   1400.5   3579.2   3614.3   1402.2
    
         8     1024   141312 |     26.9     32.4     33.8     26.7
        32     1024   141312 |     26.9     33.0     33.8     27.1
       128     1024   141312 |     27.1     32.3     32.2     30.1
       256     1024   141312 |     27.1     32.8     32.1     27.2
       512     1024   141312 |     27.2     32.6     32.3     27.0
      1024     1024   141312 |     30.0     32.8     34.0     27.5
      2048     1024   141312 |     27.1     32.7     34.7     27.6
    
         8   136192     1024 |    148.6    159.1    159.0    147.8
        32   136192     1024 |    161.2    177.9    177.3    154.4
       128   136192     1024 |    313.8    432.9    432.2    312.6
       256   136192     1024 |    370.2    613.7    613.4    370.5
       512   136192     1024 |    469.2    968.9    978.8    466.3
      1024   136192     1024 |    729.7   1735.3   1736.7    734.3
      2048   136192     1024 |   1350.1   3449.1   3419.6   1352.3
    
    Test Plan: OSS CI
    
    Reviewed By: RandySheriff
    
    Differential Revision: D104880058

--- torch/sparse/semi_structured.py
+++ torch/sparse/semi_structured.py
@@ -699,7 +699,7 @@ class SparseSemiStructuredTensorCUSPARSELT(SparseSemiStructuredTensor):
                 self.shape[0],
                 constraints.dense_min_rows,
                 constraints.dense_min_cols,
-                self.fuse_transpose_cusparselt,
+                should_transpose_dense,
                 self.alg_id_cusparselt,
                 should_transpose_dense,
             )
@@ -746,12 +746,13 @@ def _ensure_cutlass_mm_registered():
             res = torch._sparse_semi_structured_addmm(bias, packed, meta, mm_input)
         if need_pad:
             out_cols = m if should_transpose_dense else n
-            return (
+            res = (
                 res[:out_features]
                 .narrow(1, 0, out_cols)
                 .clone(memory_format=torch.contiguous_format)
             )
-        return res.contiguous()
+            return res.t().contiguous() if should_transpose_dense else res
+        return res.t().contiguous() if should_transpose_dense else res
 
     @cutlass_mm.register_fake
     def _cutlass_mm_fake(
@@ -812,14 +813,12 @@ def _ensure_cusparselt_mm_registered():
             transpose_result=fuse_transpose,
             alg_id=alg_id,
         )
-        if fuse_transpose:
-            res = res.t()
         if need_pad:
-            out_cols = m if should_transpose_dense else n
-            return res.narrow(1, 0, out_cols).clone(
-                memory_format=torch.contiguous_format
-            )
-        return res.contiguous()
+            if should_transpose_dense:
+                return res.narrow(0, 0, m)
+            else:
+                return res.narrow(1, 0, n)
+        return res
 
     @cusparselt_mm.register_fake
     def _cusparselt_mm_fake(
@@ -834,9 +833,17 @@ def _ensure_cusparselt_mm_registered():
         should_transpose_dense: bool,
     ) -> torch.Tensor:
         out_cols = dense.shape[0] if should_transpose_dense else dense.shape[1]
-        return torch.empty(
-            out_features,
-            out_cols,
-            dtype=dense.dtype,
-            device=dense.device,
-        )
+        if should_transpose_dense:
+            return torch.empty(
+                out_cols,
+                out_features,
+                dtype=dense.dtype,
+                device=dense.device,
+            )
+        else:
+            return torch.empty(
+                out_features,
+                out_cols,
+                dtype=dense.dtype,
+                device=dense.device,
+            )
